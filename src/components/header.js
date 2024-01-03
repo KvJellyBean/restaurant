690f2f8c95084712ac673d7a8d653e64fc608a9e
KvJellyBean <kevinnatan91@gmail.com>
2024-01-03 19:53:51 +0700

    Provide base components
    Add pageLoad, header, home, menu, contact js file for each components of the web page

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+export default initHeader = () => {
+    const headerTag = document.createElement('header');
+
+    // Make the logo 
+    const logoContainer = document.createElement('div');
+    
+    
+    const navbar = document.createElement('nav');
+    
+}
