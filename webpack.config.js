ca02e2e36fbfabaf80be732d8168b8da5882f651
KvJellyBean <kevinnatan91@gmail.com>
2024-01-03 19:52:19 +0700

    Add module rules and source map

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,25 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|png|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|ttf|eog)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    }
 }
